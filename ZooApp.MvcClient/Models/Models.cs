ed8a417feff77563f5efd1bcf113e20b211ff2a2
mustafizur8888 <mustafizur8888@gmail.com>
2016-09-17 08:02:34 +0600

    change home page

---
+++ ZooApp.MvcClient/Models/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ZooApp.MvcClient.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+       
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        
+    }
+}
